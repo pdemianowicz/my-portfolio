e3ecb2a1a44f213eded832d096eea95f46a018bb
pdemianowicz <pdemianowicz@gmail.com>
2025-10-23 19:10:27 +0200

    feat(layout): add header component

---
+++ app/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div className="flex items-center max-w-6xl mx-auto p-4">
+      <span className="mr-12">logo</span>
+      <nav>
+        <ul className="flex items-center gap-4">
+          <li>link</li>
+          <li>link</li>
+          <li>link</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
